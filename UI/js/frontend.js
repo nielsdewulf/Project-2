6820290b2018e235d3680eeb01df603ef4ddc571
Niels <niels.dewulf2@student.howest.be>
2020-01-09 23:54:03 +0100

    Fixed page navigation issue

--- UI/js/frontend.js
+++ UI/js/frontend.js
@@ -208,13 +208,13 @@ let buttonListeners = function() {
 		});
 	});
 	/* Start Page Scoreboard Button Event Listener */
-    
+
 	let scoreboardButtonStartpage = document.querySelector('.js-button__scoreboard-startpage');
-    scoreboardButtonStartpage.addEventListener('click', function () {
-        console.log('Singleplayer button clicked');
-        document.querySelector('.js-main__start').classList.add('c-hidden');
-        document.querySelector('.js-main__scoreboard').classList.remove('c-hidden');
-    })
+	scoreboardButtonStartpage.addEventListener('click', function() {
+		console.log('Singleplayer button clicked');
+		document.querySelector('.js-main__start').classList.add('c-hidden');
+		document.querySelector('.js-main__scoreboard').classList.remove('c-hidden');
+	});
 
 	/** New Lobby Button Event Listener*/
 
@@ -253,7 +253,7 @@ let buttonListeners = function() {
 	let startpageButtonLobby = document.querySelector('.js-button__lobby-startpage');
 	startpageButtonLobby.addEventListener('click', function() {
 		console.log('Startpage Button Clicked');
-		document.querySelector('.js-main__lobby').classList.add('c-hidden');
+		document.querySelector('.js-main__lobbychoice').classList.add('c-hidden');
 		document.querySelector('.js-main__start').classList.remove('c-hidden');
 	});
 
@@ -279,8 +279,6 @@ let buttonListeners = function() {
 		console.log('Results Button Clicked');
 		document.querySelector('.js-main__score-popup').classList.add('c-hidden');
 	});
-
-	
 };
 
 const initFrontend = () => {};
