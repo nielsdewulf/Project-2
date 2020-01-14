090d4fb7a038379a119e642e83a58fb363c07775
Niels <niels.dewulf2@student.howest.be>
2020-01-14 22:03:16 +0100

    Fixed issue where modeid would reset

--- FunctionApp/models/Game.cs
+++ FunctionApp/models/Game.cs
@@ -4,12 +4,12 @@ namespace afloat.models
 {
     public class Game
     {
-        public Guid GameId { get; set; }
-        public int PlayerCount { get; set; }
-        public int Status { get; set; }
-        public DateTime DateTime { get; set; } = DateTime.Now;
-        public int MenuId { get; set; }
-        public int ModeId { get; set; }
+        public Guid? GameId { get; set; }
+        public int? PlayerCount { get; set; }
+        public int? Status { get; set; }
+        public DateTime? DateTime { get; set; }
+        public int? MenuId { get; set; }
+        public int? ModeId { get; set; }
 
 
     }
--- FunctionApp/models/Highscore.cs
+++ FunctionApp/models/Highscore.cs
@@ -4,10 +4,10 @@ namespace afloat.models
 {
     public class Highscore
     {
-        public Guid PlayerId { get; set; }
+        public Guid? PlayerId { get; set; }
         public string Name { get; set; }
         public Guid? GameId { get; set; }
-        public int Score { get; set; }
-        public int Avatar { get; set; }
+        public int? Score { get; set; }
+        public int? Avatar { get; set; }
     }
 }
